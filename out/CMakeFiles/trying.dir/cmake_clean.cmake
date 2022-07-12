21daa3d51290fac98fa957cfa1adf5f1bec102ae
trafector67890@gmail.com <trafector67890@gmail.com>
2022-07-12 14:49:48 -0700

    alors peut-être ?

---
+++ out/CMakeFiles/trying.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trying.dir/HelloWorld.c.o"
+  "trying"
+  "trying.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/trying.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
